604552badc3bb452cb4ce525a330c279120b13a4
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2017-04-20 14:04:11 +0300

    testit toimii

--- spec/features/clubs_page_spec.rb
+++ spec/features/clubs_page_spec.rb
@@ -2,86 +2,114 @@ require 'rails_helper'
 
 
 describe "Club" do
-  before :each do
 
-    club = FactoryGirl.create :club
-    player = FactoryGirl.create :player
-    player2 = FactoryGirl.create :player2
-    player3 = FactoryGirl.create :player3
-    clubowner = FactoryGirl.create :clubowner
-    membership = FactoryGirl.create :membership
-    membership2 = FactoryGirl.create :membership2
-    player.clubowner = true
-    player.save
 
-    club.players << player
-    club.players << player3
-    club.player_id = player.id
-    club.save
+  describe "when club has one player" do
+    before :each do
+      club = FactoryGirl.create :club
+      player = FactoryGirl.create :player
+      clubowner = FactoryGirl.create :clubowner
+      membership = FactoryGirl.create :membership
+      membership2 = FactoryGirl.create :membership2
+      player.clubowner = true
+      player.save
+
+      club.player_id = player.id
+      club.save
+    end
+
+    it "can be destroyed if there is one player" do
+
+    end
   end
 
-  it "Lists clubs correctly" do
-    visit clubs_path
-    expect(page).to have_content "Listing Clubs"
-    expect(page).to have_content "ttclub"
-  end
+  describe "when club has several players" do
+    before :each do
+      club = FactoryGirl.create :club
+      player = FactoryGirl.create :player
+      player2 = FactoryGirl.create :player2
+      player3 = FactoryGirl.create :player3
+      clubowner = FactoryGirl.create :clubowner
+      membership = FactoryGirl.create :membership
+      membership2 = FactoryGirl.create :membership2
+      player.clubowner = true
+      player.save
 
-  it "can be created" do
-    sign_in("ClubOwner", "Salis")
-    visit clubs_path
-    click_link "New Club"
-    fill_in('Name', with: "DogeClub")
-    fill_in('City', with: "Herttoniemi")
-    click_button("Create Club")
-    expect(Club.count).to eq(2)
-    expect(page).to have_content "Club was successfully created."
-  end
+      club.players << player
+      club.players << player3
+      club.player_id = player.id
+      club.save
+    end
 
-  it "can have more players added" do
-    sign_in("JaakkoJaakkonen", "Salis")
-    click_link("Add players to your club")
-    select("Pekka Pekkanen", from: 'membership[player_id]')
-    click_button "Create Membership"
-    expect(page).to have_content "Membership was successfully created."
-    club = Club.first
-    expect(club.players.size).to eq (3)
-  end
+    it "Lists clubs correctly" do
+      visit clubs_path
+      expect(page).to have_content "Listing Clubs"
+      expect(page).to have_content "ttclub"
+    end
+
+    it "can be created" do
+      sign_in("ClubOwner", "Salis")
+      visit clubs_path
+      click_link "New Club"
+      fill_in('Name', with: "DogeClub")
+      fill_in('City', with: "Herttoniemi")
+      click_button("Create Club")
+      expect(Club.count).to eq(2)
+      expect(page).to have_content "Club was successfully created."
+    end
+
+    it "can have more players added" do
+      sign_in("JaakkoJaakkonen", "Salis")
+      click_link("Add players to your club")
+      select("Pekka Pekkanen", from: 'membership[player_id]')
+      click_button "Create Membership"
+      expect(page).to have_content "Membership was successfully created."
+      club = Club.first
+      expect(club.players.size).to eq (3)
+    end
 
 
 
 
-  it "shows right clubowner" do
-    visit_ttclub_page
-    expect(page).to have_content "Clubowner: Jaakko Jaakkonen"
-  end
+    it "shows right clubowner" do
+      visit_ttclub_page
+      expect(page).to have_content "Clubowner: Jaakko Jaakkonen"
+    end
 
-  it "can be edited by clubowner" do
-    sign_in("JaakkoJaakkonen" , "Salis")
-    visit_ttclub_page
-    click_link 'Edit Club'
-    fill_in('Name',with: "newname")
-    click_button('Update Club')
-    expect(page).to have_content "Club was successfully updated."
-    expect(page).to have_content "newname"
-  end
+    it "can be edited by clubowner" do
+      sign_in("JaakkoJaakkonen" , "Salis")
+      visit_ttclub_page
+      click_link 'Edit Club'
+      fill_in('Name',with: "newname")
+      click_button('Update Club')
+      expect(page).to have_content "Club was successfully updated."
+      expect(page).to have_content "newname"
+    end
 
-  it "can have more players if clubowner add's them" do
-    sign_in("JaakkoJaakkonen", "Salis")
-    click_link "Add players to your club"
-    select('Pekka Pekkanen', from:'membership[player_id]')
-    click_button "Create Membership"
-    expect(page).to have_content "Membership was successfully created."
-    expect(page).to have_content "Total of players: 3"
-  end
+    it "can have more players if clubowner add's them" do
+      sign_in("JaakkoJaakkonen", "Salis")
+      click_link "Add players to your club"
+      select('Pekka Pekkanen', from:'membership[player_id]')
+      click_button "Create Membership"
+      expect(page).to have_content "Membership was successfully created."
+      expect(page).to have_content "Total of players: 3"
+    end
 
-  it "can be left" do
-     sign_in("MattiLuukkainen", "Salis")
-     visit_ttclub_page
-     click_button 'Leave this club'
-     expect(page).to have_content "Membership was successfully destroyed."
-     visit_ttclub_page
-     expect(page).to have_content "Total of players: 1"
-  end
+    it "can be left" do
+       sign_in("MattiLuukkainen", "Salis")
+       visit_ttclub_page
+       click_button 'Leave this club'
+       expect(page).to have_content "Membership was successfully destroyed."
+       visit_ttclub_page
+       expect(page).to have_content "Total of players: 1"
+    end
+
+
+    end
+
+
+
+
 
   def register_as(player)
     visit signup_path
@@ -102,6 +130,12 @@ describe "Club" do
   def visit_ttclub_page
     visit clubs_path
     click_link "ttclub"
+  end
+
+  def make_jaakko_clubowner
+    club.players << player
+    club.player_id = player.id
+    club.save
   end
 
 
